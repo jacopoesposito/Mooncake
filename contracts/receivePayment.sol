ffae300f0c531a9f254a9337b4a8d8c05ca199b0
jacopoesposito <jaco.espo@hotmail.it>
2021-12-21 19:56:26 +0100

    Aggiunte le funzioni di pagamento con e senza saldo

--- contracts/receivePayment.sol
+++ contracts/receivePayment.sol
@@ -1,20 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 < 0.9.0;
 
+contract storePaymentsDiscountB {
+
+  function addPayment(uint _discount, address _idwallet) public {}
+  function setBalance(address _idwallet, uint _discount) public {}
+  function checkBalance(address _idwallet) public returns (uint discount) {}
+
+}
+
 contract receivePayment {
-  address public owner;
-  uint private value;
-  bytes private message;
+  /*uint public idwallet;
+  uint private price;*/
+  uint private discount;
+  
 
 
-  constructor() payable{
+  constructor() {
     
   }
 
-  function receivePay() public payable {
-    value = msg.value;
-    message = msg.data;
-    owner = msg.sender;
+  storePaymentsDiscountB sPD;
+
+  function Existing(address _t) public {
+      sPD = storePaymentsDiscountB(_t);
+  }
+  
+
+  function receivePay(uint _price, address _t) public payable {
+    require(msg.value == _price*10**18, 'Need to send enough ether'); //Need to convert _price in wei before the comparison
+
+    discount = (_price*10)/100; //Set the discount percentage
+    Existing(_t); //Creating a new instance of the smart contract StorePaymentDiscount 
+    sPD.addPayment(discount, msg.sender); //Adding the information about the payment
+  }
+
+  function payWithBalance(uint _price, address _t) public payable{
+
+    Existing(_t);
+    uint _balance = sPD.checkBalance(msg.sender);
+    if((_price - _balance) >= 0){ //Check that the balance is enough
+      //uint _newPrice = _price - _balance;
+      sPD.setBalance(msg.sender, 0); //Set the balance to 0
+      payable(msg.sender).transfer(_balance*1 ether); //Return the money saved to the EOA
+
+    }else{
+      revert("There are some problems with your balance");
+    }
   }
 
 
--- contracts/storePaymentsDiscount.sol
+++ contracts/storePaymentsDiscount.sol
@@ -2,22 +2,22 @@ pragma solidity >=0.4.22 < 0.9.0;
 
 contract storePaymentsDiscount {
     struct InfoPayment {
-        uint idwallet;
+        address idwallet;
         uint discount;
         bool exists;
     }
 
-    mapping(uint => InfoPayment) public payments;
+    mapping(address => InfoPayment) public payments;
     InfoPayment[] paymentArray;
 
     constructor() {
-        addPayment(1, 2);
-        addPayment(2, 3);
+        addPayment(1, 0x5B5922E8BbcE08B19D15005a899A4E330096C940);
+        addPayment(2, 0x6262D676b040E8dc2989dE00B3ad4D60A9eF07d0);
     }
     
     uint public paymentsCount;
 
-    function addPayment(uint _discount, uint _idwallet) public {
+    function addPayment(uint _discount, address _idwallet) public {
         if(!payments[_idwallet].exists){ //Checking if the balance is already present 
             payments[_idwallet] = InfoPayment(_idwallet, _discount, true); //If not, create the balance 
             paymentArray.push(payments[_idwallet]);
@@ -32,7 +32,7 @@ contract storePaymentsDiscount {
         }
     }
 
-    function setBalance(uint _idwallet, uint _discount) public{
+    function setBalance(address _idwallet, uint _discount) public{
         for(uint i = 0; i < paymentArray.length; i++){
             if(paymentArray[i].idwallet == _idwallet){
                     paymentArray[i].discount = _discount; //Set the balance to the value _discount
@@ -40,7 +40,7 @@ contract storePaymentsDiscount {
         }
     }
 
-    function checkBalance(uint _idwallet) public returns (uint discount){
+    function checkBalance(address _idwallet) public view returns (uint discount){
         for(uint i = 0; i < paymentArray.length; i++){
             if(paymentArray[i].idwallet == _idwallet){
                 return paymentArray[i].discount;
